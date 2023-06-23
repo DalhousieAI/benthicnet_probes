DEV/BUG: Changed loss calculation fixed bug in interpreting logit masking.

utils/benthicnet_dataset.py
@@ -55,36 +55,7 @@ class BenthicNetDataset(torch.utils.data.Dataset):
 
         path = row["dataset"] + "/" + row["site"] + "/" + img_name
         node_file_path = os.path.join(os.environ["SLURM_TMPDIR"], path)
-        if os.path.isfile(node_file_path):
-            sample = PIL.Image.open(node_file_path)
-        else:
-            # Need to load the file from the tarball over the network
-            with tarfile.open(
-                os.path.join(self.tar_dir, row["tarname"]), mode="r"
-            ) as t:
-                # print(row)
-                sample = PIL.Image.open(t.extractfile(path))
-                # PIL.Image has lazy data loading. But PIL won't be able to access
-                # the data from the tarball once we've left this context, so we have
-                # to manually trigger the loading of the data now.
-                sample.load()
-                # print(time.time() - start_time)
-
-            # Other workers might try to access the same image at the same
-            # time, creating a race condition. If we've started writing the
-            # output, there will be a partially written file which can't be
-            # loaded. To avoid another worker trying to read our partially
-            # written file, we write the output to a temp file and
-            # then move the file to the target location once it is done.
-            with tempfile.TemporaryDirectory() as dir_tmp:
-                # Write to a temporary file
-                node_file_temp_path = os.path.join(dir_tmp, os.path.basename(path))
-                sample.save(node_file_temp_path)
-                # Move our temporary file to the destination
-                os.makedirs(os.path.dirname(node_file_path), exist_ok=True)
-                shutil.move(node_file_temp_path, node_file_path)
-        # load_time = time.time() - start_time
-        # print("load time:", load_time)
+        sample = PIL.Image.open(node_file_path)
 
         if self.transform:
             sample = self.transform(sample)

utils/utils.py
@@ -13,7 +13,6 @@ from networkx import relabel_nodes
 from omegaconf import OmegaConf
 from PIL import Image
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
-from solo.utils.lars import LARS
 from torch.optim.lr_scheduler import (
     ExponentialLR,
     MultiStepLR,
@@ -53,7 +52,6 @@ _HEADER_ROOT_DICT = {
 
 _OPTIMIZERS = {
     "sgd": torch.optim.SGD,
-    "lars": LARS,
     "adam": torch.optim.Adam,
     "adamw": torch.optim.AdamW,
 }
@@ -91,7 +89,7 @@ def parser():
     parser.add_argument(
         "--tar_dir",
         type=str,
-        default="/project/def-ttt/become/benthicnet-compiled/compiled_labelled_512px/tar/",
+        default="/gpfs/project/6012565/become_labelled/compiled_labelled_512px/tar",
         help="set directory for training tar file",
     )
     parser.add_argument(
@@ -226,20 +224,20 @@ def parse_heads_and_masks(sample_row, Rs):
         non_hierarchical_head = isinstance(Rs[root], int)
         if isinstance(raw_indices, str) and len(raw_indices) > 0:
             indices = np.array(ast.literal_eval(raw_indices))
-            if "mask" in header:
-                lab_array = torch.ones(len(Rs[root][0]), dtype=torch.half)
+            if "Mask" in header:
+                lab_array = torch.ones(len(Rs[root][0]))
                 lab_array[indices] = 0
             else:
                 if non_hierarchical_head:
-                    lab_array = torch.zeros(Rs[root], dtype=torch.half)
+                    lab_array = torch.zeros(Rs[root])
                 else:
-                    lab_array = torch.zeros(len(Rs[root][0]), dtype=torch.half)
+                    lab_array = torch.zeros(len(Rs[root][0]))
                 lab_array[indices] = 1
         else:
             if non_hierarchical_head:
-                lab_array = torch.full((Rs[root],), float("nan"))
+                lab_array = torch.zeros(Rs[root])
             else:
-                lab_array = torch.full((len(Rs[root][0]),), float("nan"))
+                lab_array = torch.zeros(len(Rs[root][0]))
         sample_row[header] = lab_array
     return sample_row
 